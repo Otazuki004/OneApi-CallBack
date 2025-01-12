51039b7f5215430cd340b27feaa96adc024fdfb5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 13:49:53 +0530

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,21 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
+import logging
+from variables import *
+import os
+from quart import Quart
+from quart_cors import cors
 
+
+logging.basicConfig(
+    format="[API] %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+
+# VARIABLES
+MONGO_DB_URL = os.environ.get("MONGO_DB_URL") or VAR_MONGO_DB_URL
+
+DATABASE = AsyncIOMotorClient(MONGO_DB_URL)["CallbackApi"]
+app = Quart(__name__)
