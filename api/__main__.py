7d2419f70d72dadbcc34e064d21526f424dccf9f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 13:48:11 +0530

    Update __main__.py

--- api/__main__.py
+++ api/__main__.py
@@ -1,1 +1,15 @@
+import os
+from api import app
+from quart_cors import cors
+import asyncio
+from quart import request, jsonify, Blueprint
 
+app = cors(app, allow_origin="*")
+
+@app.route('/')
+def home():
+    return jsonify({'success': 'server online'})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
