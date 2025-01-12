fd773e16d1897c41aaa41eed7633072f2550cc34
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 15:04:43 +0530

    Update __main__.py

--- api/__main__.py
+++ api/__main__.py
@@ -14,5 +14,5 @@ def home():
     return jsonify({'success': 'server online'})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = 8080
     app.run(debug=True, host="0.0.0.0", port=port)
