9c6386e225e652954514c2ee1feaa502be787841
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 16:04:40 +0530

    Update db.py

--- api/db/db.py
+++ api/db/db.py
@@ -4,8 +4,9 @@ import logging
 cb = DATABASE['cb']
 
 class db:
-  async def add(self, user_id: int, token):
+  async def add(self, user_id, token):
     db = cb
+    user_id = int(user_id)
     user = await db.find_one({"_id": user_id})
     if user: return
     await db.update_one({"_id": 1}, {"$addToSet": {"users": user_id}}, upsert=True)
